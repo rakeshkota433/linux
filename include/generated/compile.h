10ef518182436e0c51f1b20ff5b2d91991f677f9
rakesh <rakhy.kota433@gmail.com>
2018-09-27 00:32:36 +0530

    inc

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 77 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#77 SMP PREEMPT Sat Apr 21 11:57:49 IST 2018"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "atilpunpc107"
+#define LINUX_COMPILER "gcc version 5.3.0 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.1.15-2.0.1+gb63f3f5"
